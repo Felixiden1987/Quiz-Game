81ea6a22f296d04977f5eebd6af2e1a2b9a75498
Felixiden1987 <ouc98oec@students.codeinstitute.net>
2024-09-17 09:43:17 +0000

    add questions for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+/**
+ * Questions for the quiz
+ */
 const questions = [
     {
-        question: "What is love?",
+        question: "What is love? ___",
         answers: [
             { text: "Baby don't hurt me", correct: true },
             { text: "Baby u hurt me", correct: false },
@@ -9,145 +12,149 @@ const questions = [
         ]
     },
     {
-        question: "What is love?",
+        question: "Never gonna ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Give you up", correct: false },
+            { text: "Let you down", correct: false },
+            { text: "Tell a lie", correct: false },
+            { text: "All of the above", correct: true },
         ]
     },
     {
-        question: "What is love?",
+        question: "I said maybe, you're gonna be the one that ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Dates", correct: false },
+            { text: "Loves", correct: false },
+            { text: "Shaves", correct: false },
+            { text: "Saves", correct: true },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "You are ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Desire", correct: false },
+            { text: "A liar", correct: false },
+            { text: "My fire", correct: true },
+            { text: "In a choir", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "I really, really, really wanna ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Zig a zig ah", correct: true },
+            { text: "Zig a zig zah", correct: false },
+            { text: "Zig a zag ah", correct: false },
+            { text: "Zig a zig now", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "You're simply the best, better than all the rest, better than ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Everyone", correct: false },
+            { text: "Anyone", correct: true },
+            { text: "Havin' fun", correct: false },
+            { text: "Someone", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Tommy used to work ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "On the docks", correct: true },
+            { text: "On the cars", correct: false },
+            { text: "At the bar", correct: false },
+            { text: "At the barn", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Dancing queen, feel the beat from the ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Quarantine", correct: false },
+            { text: "Funky Scene", correct: false },
+            { text: "Tambourine", correct: true },
+            { text: "Skinny Jeans", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Do you remember the ___ night of September?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Very First", correct: false },
+            { text: "21st", correct: true },
+            { text: "23rd", correct: false },
+            { text: "24th", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "I love rock 'n roll, just put another ___ in the jukebox baby?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Dime", correct: true },
+            { text: "Buck", correct: false },
+            { text: "Coin", correct: false },
+            { text: "Penny", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Beat it, beat it, beat it, beat it, noone want's to be ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Conceited", correct: false},
+            { text: "Deleted", correct: false },
+            { text: "Unseated", correct: false },
+            { text: "Defeated", correct: true },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Oh, i wanna dance with somebody, i wanna feel the ___ with somebody?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Heat", correct: true },
+            { text: "Seat", correct: false },
+            { text: "Meat", correct: false },
+            { text: "Treat", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Here we are now ___ ?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "we're contagius", correct: false },
+            { text: "Entertain us", correct: true },
+            { text: "We're strangers", correct: false },
+            { text: "We're dangerous", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Don't go chasin' waterfalls, listen to the ___ that you're used to?",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Rivers and the streams", correct: true },
+            { text: "Rivers and the seas", correct: false },
+            { text: "Rivers and the trees", correct: false },
+            { text: "Rivers and the lakes", correct: false },
 
         ]
     },
     {
-        question: "What is love?",
+        question: "Hold me closer, Tiny Dancer?, count the ___ on the highway",
         answers: [
-            { text: "Baby don't hurt me", correct: true },
-            { text: "Baby u hurt me", correct: false },
-            { text: "Baby you hurt me", correct: false },
-            { text: "Baby just hurt me", correct: false },
+            { text: "Street lights", correct: false },
+            { text: "Headlights", correct: true },
+            { text: "Bar-fights", correct: false },
+            { text: "Car-lights", correct: false },
 
         ]
     },
 ];
+/**
+ * variables for elements 
+ */
+
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("btn-next");
@@ -156,19 +163,4 @@ let currentQuestionIndex = 0;
 let score = 0;
 
 
-function incrementScore() {
 
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-
-}
-
-/**
- * Gets the current tally of incorrect answers from the DOM and increments it by 1
- */
-function incrementWrongAnswer() {
-
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-    
-}
