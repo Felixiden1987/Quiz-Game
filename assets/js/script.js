8bb2cf5b2f5e537f066e08481b1e27d5b318285c
Felixiden1987 <ouc98oec@students.codeinstitute.net>
2024-09-17 12:00:24 +0000

    add function to display score after finishing quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,6 +225,26 @@ function selectAnswer(e) {
     });
     nextButton.style.display = "block";
 }
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+}
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 /**
  * Calls the function to start quiz
